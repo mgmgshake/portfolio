a705389adf525d62acc8ca7c787451c6bd0f701e
mgmgshake <pocky722+git@gmail.com>
2021-10-09 16:03:46 +0900

    feat: gitのurl変更

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -12,7 +12,7 @@ const Header = () => {
             <h2 className={styles.title}>Hello!<br />I'm shakexxmmm.</h2>
             <ul className={styles.snsLink}>
                 <li><a href="https://twitter.com/mgmgshake" target="_blank" rel="noreferrer"><img width={50} height={50} src="/sns/twitter.png" alt="twitter" /></a></li>
-                <li><a href="https://github.com/shakexxmmm" target="_blank" rel="noreferrer"><img width={50} height={50} src="/sns/github.png" alt="twitter" /></a></li>
+                <li><a href="https://github.com/mgmgshake" target="_blank" rel="noreferrer"><img width={50} height={50} src="/sns/github.png" alt="twitter" /></a></li>
             </ul>
         </div>
     )
