952c40b2f96bae312822abd15148ad8777d13fe3
mgmgshake <pocky722+git@gmail.com>
2021-10-17 17:26:26 +0900

    feat: worksに記事が反映できるように修正

--- src/components/top/Works.js
+++ src/components/top/Works.js
@@ -1,16 +1,38 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { useStaticQuery, graphql, Link } from "gatsby";
 import "../../css/index.css";
 import WorksCord from "./WorksCard";
 
-const About = () => {
+const Works = () => {
+    const data = useStaticQuery(
+        graphql`
+            query {
+                allMicrocmsWorks {
+                    nodes {
+                        description
+                        slug
+                        title
+                        thumbnail {
+                            url
+                        }
+                    }
+                }
+            }  
+        `
+    ) 
+    console.log(data)
     return (
         <section className="bg-yellow-300 py-40">
             <h2 className="text-6xl mb-2 text-center font-title">WORKS</h2>
             <p className="text-base mb-10 text-center">制作物</p>
-            <WorksCord />
+            <ul>
+                { data.allMicrocmsWorks.nodes.map(work => {
+                    return <li><WorksCord work={work} /></li>
+                })}
+            </ul>
         </section>
     )
 }
 
-export default About
+
+export default Works
--- src/components/top/WorksCard.js
+++ src/components/top/WorksCard.js
@@ -2,21 +2,22 @@ import * as React from "react";
 import { Link } from "gatsby";
 import "../../css/index.css";
 
-const WorksCard = () => {
+const WorksCard = (props) => {
+    const { title, description, thumbnail, slug } = props.work
     return (
-        <div className="relative container mx-auto">
+        <div className="relative container max-w-5xl mx-auto">
             <div className="rounded-2xl bg-white w-9/12">
-                <img className="w-full h-auto" src="https://placehold.jp/1920x1080.png" alt="サムネイル" />
+                <img className="w-full h-auto" src={thumbnail?.url} alt={title} />
             </div>
             <div class="p-10 rounded-2xl bg-white shadow-card w-4/12 absolute right-0 -bottom-10">
-                <h3 className="text-2xl text-gray-800 font-bold mb-3">やまだ食堂</h3>
+                <h3 className="text-2xl text-gray-800 font-bold mb-3">{title}</h3>
                 <ul className="flex -mx-1 mb-4">
                     <li className="text-xs px-3 py-2 text-white bg-blue-500 rounded-2xl mx-1">LP制作</li>
                     <li className="text-xs px-3 py-2 text-white bg-blue-500 rounded-2xl mx-1">デザイン</li>
                     <li className="text-xs px-3 py-2 text-white bg-blue-500 rounded-2xl mx-1">コーディング</li>
                 </ul>
-                <p className="text-gray-700 leading-relax mb-4">説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。</p>
-                <Link to="/" className="text-blue-500 float-right">view more</Link>
+                <p className="text-gray-700 leading-relax mb-4">{description}</p>
+                <Link to={`/works/${slug}`} className="text-blue-500 float-right">view more</Link>
             </div>
         </div>
     )
