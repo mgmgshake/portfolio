c04e2f288e11504f4db27ce7803037b42366c983
mgmgshake <pocky722+git@gmail.com>
2021-10-17 22:10:44 +0900

    feat: 制作物詳細ページを作成

---
+++ src/components/works/WorkContent.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const WorkContent = (props) => {
+  const { title, description, detail, slug, thumbnail } = props.post;
+  return (
+      <article className="container bg-white p-16 rounded-2xl shadow-card mx-auto">
+        <div>
+          <p className="w-full mb-14">
+            <img src={thumbnail?.url} alt={title} className="w-full h-auto" />
+          </p>
+          <h2 className="text-3xl font-bold mb-3">{ title }</h2>
+          <p className="text-xs mb-10">{description}</p>
+          <div className="text-base leading-relaxed cms-contents" dangerouslySetInnerHTML={{ __html: detail }}></div>
+        </div>
+      </article>
+  )
+}
+
+export default WorkContent
--- src/pages/works/{MicrocmsWorks.slug}.js
+++ src/pages/works/{MicrocmsWorks.slug}.js
@@ -1,11 +1,9 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 import { Helmet } from "react-helmet"
-import PostContent from "../../components/works/PostContent"
+import WorkContent from "../../components/works/WorkContent"
 import Layout from "../../components/Layout"
-import "../common.scss"
 
-// markup
 const PostPage = (props) => {
   const post = props.data.microcmsWorks
   return (
@@ -14,7 +12,11 @@ const PostPage = (props) => {
         <title>{ post.title }</title>
         <meta name="description" content={`${post.title}`} />
       </Helmet>
-      <PostContent post={post} />
+      <main className="bg-yellow-300 py-20">
+        <h1 className="text-6xl mb-2 text-center font-title">WORKS</h1>
+        <p className="text-base mb-10 text-center">制作物</p>
+        <WorkContent post={post} />
+      </main>
     </Layout>
   )
 }
