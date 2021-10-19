c127c8c6cc25df24ed0866fa5552c2000b89e668
mgmgshake <pocky722+git@gmail.com>
2021-10-19 23:13:32 +0900

    feat: div→sectionに変更

--- src/pages/works/{MicrocmsWorks.slug}.js
+++ src/pages/works/{MicrocmsWorks.slug}.js
@@ -12,11 +12,11 @@ const PostPage = (props) => {
         <title>{ post.title } | Maki Goto</title>
         <meta name="description" content={`${post.title}`} />
       </Helmet>
-      <div className="bg-yellow-300 py-20">
+      <section className="bg-yellow-300 py-20">
         <h1 className="midashi mb-2">WORKS</h1>
         <p className="text-base mb-10 text-center">制作物</p>
         <WorkContent post={post} />
-      </div>
+      </section>
     </Layout>
   )
 }
