06bf71fd37f4235f60cfcf7b7ed3a69f22de7cc0
mgmgshake <pocky722+git@gmail.com>
2022-02-22 19:06:54 +0900

    add 404ページ作成

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,54 +1,29 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import Head from "../components/Head";
+import Layout from "../components/Layout";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
+const NotFoundPage: React.FC = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <>
+      <Head title="404" />
+      <Layout>
+        <div className="ly_cont">
+          <div className="bl_404">
+            <p className="bl_404_image">
+              <img
+                src="/icon/icon_sad.svg"
+                alt="icon"
+                width="240"
+                height="240"
+              />
+            </p>
+            <h1>404</h1>
+            <p>お探しのページはないみたいです。すみません。</p>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
