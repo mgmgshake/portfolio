a27c610509c50d46a446a6911670cf5cf878ae75
mgmgshake <pocky722+git@gmail.com>
2022-02-22 18:51:03 +0900

    add layoutコンポーネント 編集

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,16 @@
 import * as React from "react";
+import Header from "./Header";
 import Footer from "./Footer";
 import "../css/index.css";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <>
-      <div className="font-body">
+      <div className="ly_wrapper">
+        <Header />
         <main>{children}</main>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 };
